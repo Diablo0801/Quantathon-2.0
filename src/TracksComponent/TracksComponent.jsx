5f560e65064097dc4aae24d3be34b552dbcdbb4c
Pulkit Shringi <pulkitshringi@yahoo.com>
2024-10-08 21:45:57 +0530

    updated tracks description & added prizes

--- src/TracksComponent/TracksComponent.jsx
+++ src/TracksComponent/TracksComponent.jsx
@@ -7,39 +7,41 @@ import quantumMlImage from './assets/quantum_ml.png';
 import openInnovationImage from './assets/openinnovation.png';
 
 const TracksComponent = () => {
-  const [selectedTrack, setSelectedTrack] = useState("Quantum Core");
+  const [selectedTrack, setSelectedTrack] = useState("Core Quantum");
 
   const tracks = [
     { 
-      name: "Quantum Core", 
-      description: `Quantum Core is focused on exploring the fundamental principles of quantum mechanics and their applications to develop advanced computational techniques. 
-                    Our research aims to enhance quantum algorithms and push the boundaries of current quantum systems.`,
+      name: "Core Quantum", 
+      description: `The Core Quantum Track delves into the fundamental concepts of quantum computing and quantum technologies. Participants will embark on a quest to understand the principles behind quantum mechanics, quantum algorithms, and quantum hardware. The track aims to provide a hands-on experience in working with quantum programming languages, simulators, and actual quantum devices.
+From exploring quantum superposition and entanglement to implementing quantum algorithms such as Shor's algorithm and Grover's algorithm, this track will challenge participants to stretch their minds and develop quantum solutions to real-world problems. Participants will gain insights into the unique advantages and limitations of quantum computing compared to classical computing.`,
       image: quantumCoreImage 
     },
     { 
       name: "AI/ML", 
-      description: `The AI/ML track leverages artificial intelligence and machine learning algorithms to solve complex problems in quantum computing. 
-                    We develop models that can accelerate quantum processes and bring innovation to classical machine learning systems.`,
+      description: `The AI ML Track is focused on exploring the vast potential of artificial intelligence and machine learning in solving complex challenges across various domains. Participants in this track will have the opportunity to work on cutting-edge projects, applying state-of-the-art algorithms and techniques to real-world datasets.
+From predictive analytics and natural language processing to computer vision and reinforcement learning, the AI ML Track encompasses a wide range of exciting areas in the field of AI. Participants can choose projects aligned with their interests, enabling them to delve deep into specific subdomains of AI and build their expertise.`,
       image: aiMlImage 
     },
     { 
       name: "Quantum Machine Learning", 
-      description: `Quantum Machine Learning combines quantum computing with traditional machine learning techniques to process data more efficiently. 
-                    We focus on creating quantum-enhanced algorithms that can outperform classical approaches in certain problem domains.`,
+      description: `The Quantum Machine Learning Track merges the fields of quantum computing and machine learning, exploring how quantum algorithms can enhance AI applications. Participants will gain hands-on experience in developing and optimizing quantum machine learning models, delving into topics such as quantum data encoding, variational quantum classifiers, and more.
+This track will challenge participants to build cutting-edge quantum machine learning solutions while understanding how these quantum algorithms compare to classical methods.`,
       image: quantumMlImage 
     },
     { 
       name: "Open Innovation", 
-      description: `The Open Innovation track is designed to foster collaborative research and development in quantum computing. 
-                    We encourage cross-industry partnerships to accelerate the adoption of quantum technologies and drive innovation in a rapidly evolving field.`,
+      description: `The Open Innovation Track is a blank canvas where participants are encouraged to think outside the box and tackle problems from various domains. There are no predefined themes or restrictions in this track, allowing participants to explore a wide array of topics that spark their curiosity and passion.
+Whether it's developing cutting-edge technologies, addressing societal issues, or creating novel applications, the track invites participants to let their imaginations run wild and embrace the freedom to innovate. Participants can choose to work on solo projects, form cross-functional teams, or collaborate with other participants during the Hackathon. They are encouraged to bring diverse skill sets, backgrounds, and perspectives to the table, fostering an environment of open dialogue and co-creation.`,
       image: openInnovationImage 
     }
   ];
 
   return (
-    <div id="tracks" className="flex flex-col items-center  py-10 min-h-screen">
-      <h1 className="text-[#94D92A] text-3xl sm:text-5xl font-bold mb-6 sm:mb-12">Tracks</h1>
-      
+    <div id="tracks" className="flex flex-col items-center py-10">
+      <h1 className="text-[#94D92A] text-3xl sm:text-5xl font-bold mb-6 sm:mb-12">
+        Tracks
+      </h1>
+
       <div className="flex justify-center space-x-4 sm:space-x-10 mb-8 sm:mb-12">
         {tracks.map((track, index) => (
           <button
@@ -55,9 +57,11 @@ const TracksComponent = () => {
       </div>
 
       <div className="bg-gray-800 text-white p-6 sm:p-10 rounded-lg max-w-xs sm:max-w-2xl text-center">
-        <h2 className="text-2xl sm:text-4xl font-bold mb-4 sm:mb-6 text-[#94D92A]">{selectedTrack}</h2>
-        <p className="text-base sm:text-lg sm:leading-8">
-          {tracks.find(track => track.name === selectedTrack).description}
+        <h2 className="text-2xl sm:text-4xl font-bold mb-4 sm:mb-6 text-[#94D92A]">
+          {selectedTrack}
+        </h2>
+        <p className="text-base sm:text-lg sm:leading-8 tracking-wide leading-relaxed">
+          {tracks.find((track) => track.name === selectedTrack).description}
         </p>
       </div>
     </div>
