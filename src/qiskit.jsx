218819f0c367bde9a65efcc2f8c402a0f2294813
hrishi-sarma <sarmahrishi05@gmail.com>
2024-10-08 15:42:55 +0530

    updates

--- src/qiskit.jsx
+++ src/qiskit.jsx
@@ -1,72 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './qiskit.css';
 
 const Qiskit = () => {
-  const cards = [
-    {
-      id: 1,
-      image: '/qiskit.jpg', 
-      heading: 'IBM qiskit',
-      text: 'Card 1 Description',
-    },
-    {
-      id: 2,
-      image: 'Timeline.png', 
-      heading: '',
-      text: '', 
-    }
-  ];
-
-  const [currentCardIndex, setCurrentCardIndex] = useState(0);
-
-  const handleSwipeLeft = () => {
-    setCurrentCardIndex((prevIndex) => 
-      prevIndex === cards.length - 1 ? 0 : prevIndex + 1
-    );
+  const card = {
+    id: 1,
+    image: '/qiskit.jpg',
+    heading: 'IBM qiskit',
+    text: 'Card 1 Description',
   };
-
-  const handleSwipeRight = () => {
-    setCurrentCardIndex((prevIndex) => 
-      prevIndex === 0 ? cards.length - 1 : prevIndex - 1
-    );
-  };
-
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      handleSwipeLeft();
-    }, 5000); 
-    
-    return () => clearInterval(interval); 
-  }, []);
 
   return (
-    <div  className="content">
+    <div className="content">
       <div className="header"></div>
       <div className="qiskitstuff">
         <div className="cardscontainer">
-          <div id="qiskit"
-            className="cards-slider" 
-            style={{ transform: `translateX(-${currentCardIndex * 100}%)` }}
-          >
-            {cards.map((card, index) => (
-              <div key={card.id} className="card">
-                {index === 1 ? (
-                  <img src={card.image} alt={`Card ${index + 1}`} className="full-card-image" />
-                ) : (
-                  <>
-                    <img src={card.image} alt={`Card ${index + 1}`} />
-                    <h2>{card.heading}</h2>
-                    <p>{card.text}</p>
-                  </>
-                )}
-              </div>
-            ))}
+          <div id="qiskit" className="cards-slider">
+            <div key={card.id} className="card">
+              <img src={card.image} alt="IBM qiskit" />
+              <h2>{card.heading}</h2>
+              <p>{card.text}</p>
+            </div>
           </div>
-        </div>
-        <div className="controls">
-          <button onClick={handleSwipeRight} className="swipe-button">Swipe Right</button>
-          <button onClick={handleSwipeLeft} className="swipe-button">Swipe Left</button>
         </div>
       </div>
     </div>
